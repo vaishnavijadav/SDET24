1c888247101c972ac23c54f8f128bcfbafc29687
puneeth <puneeth@DESKTOP-9U7KF4V>
2021-11-12 14:29:51 +0530

    execute project

---
+++ src/test/java/com/crm/Practise/Pracitse3Test.java
@@ -0,0 +1,15 @@
+package com.crm.Practise;
+
+import org.testng.annotations.Test;
+
+public class Pracitse3Test {
+	
+	public class Practise1Test {
+		@Test
+		public void Clea2ng()
+		{
+			System.out.println("Test case6");
+		}
+		}
+
+}
